299ee49f0b2729e3bae253c83e0474d8ec594a30
Roberto <roberto.torres.dev@gmail.com>
2024-08-29 15:55:05 -0400

    adding user creation

--- kidd_tasks/kidd_tasks/settings.py
+++ kidd_tasks/kidd_tasks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'kidd_tasks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
